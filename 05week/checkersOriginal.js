3fb3721a19145e08fcf488435b8aa94d19924be3
James Walker <james.walker@contractor.net>
2017-09-13 21:39:07 -0500

    successful conversion of all css into object, which simplified code in functions

--- 05week/checkersOriginal.js
+++ 05week/checkersOriginal.js
@@ -14,22 +14,20 @@ function Game () {
   };
 
   this.removeChecker = function (row, col) {
-    this.board.grid[row][col] = this.board.constructor.openSquare;
+    this.board.grid[row][col] = this.board.constructor.squareCss.open;
     this.board.checkers.pop;
   };
 
   this.addChecker = function (row, col) {
-    this.board.grid[row][col] = this.board.currentPlayer.checkerCSS;
+    this.board.grid[row][col] = this.board.currentPlayer.checkerCss;
     this.board.checkers.push('placeHolder');
   };
 
   this.togglePlayer = function () {
     if (this.board.currentPlayer.color === this.board.constructor.firstPlayer.color) {
-      this.board.currentPlayer.color = this.board.constructor.secondPlayer.color;
-      this.board.currentPlayer.checkerCSS = this.board.constructor.secondPlayer.checkerCSS;
+      this.board.currentPlayer = {...this.board.constructor.secondPlayer};
     } else {
-      this.board.currentPlayer.color = this.board.constructor.firstPlayer.color;
-      this.board.currentPlayer.checkerCSS = this.board.constructor.firstPlayer.checkerCSS;
+      this.board.currentPlayer = {...this.board.constructor.firstPlayer};
     }
     console.clear();
     this.board.viewGrid();
@@ -43,21 +41,15 @@ function Game () {
       const fromCol = whichPiece - fromRow * 10;
       const toRow = parseInt(toWhere / 10);
       const toCol = toWhere - toRow * 10;
-      if (this.board.grid[fromRow][fromCol] === this.board.currentPlayer.checkerCSS && this.board.grid[toRow][toCol] === this.board.constructor.openSquare) {
+      debugger
+      if (this.board.grid[fromRow][fromCol] === this.board.currentPlayer.checkerCss && this.board.grid[toRow][toCol] === this.board.constructor.squareCss.open) {
         switch (true) {
-          // FirstPlayer is moving a checker in a downwards direction into an open square
-          case this.board.currentPlayer.color === this.board.constructor.firstPlayer.color && toRow - fromRow === 1 && Math.abs(toCol - fromCol) === 1:
+          // Either player moves a checker into an open square
+          case toRow - fromRow === this.board.currentPlayer.direction && Math.abs(toCol - fromCol) === 1:
             this.removeChecker(fromRow, fromCol);
             this.addChecker(toRow, toCol);
             this.togglePlayer();
             break;
-          // SecondPlayer is moving a checker in a upwards direction into an open square
-          case this.board.currentPlayer.color === this.board.constructor.secondPlayer.color && fromRow - toRow === 1 && Math.abs(toCol - fromCol) === 1:
-            this.removeChecker(fromRow, fromCol);
-            this.addChecker(toRow, toCol);
-            this.togglePlayer();
-            break;
-
         }
       } else { console.log(`ERROR: Either your checker is not on ${whichPiece} or there is a checker on ${toWhere}. Player ${this.board.currentPlayer.color}, please submit a valid move.`); }
     } else { console.log(`ERROR: Either ${whichPiece} or ${toWhere} is out of bounds. Player ${this.board.currentPlayer.color}, please submit a valid move.`); }
@@ -68,19 +60,21 @@ function Board () {
   this.constructor = {
     name: 'Board',
     firstPlayer: {
-      color: 'Blue',  // Must be synced with firstPlayer.checkerCSS's color value
-      checkerCSS: 'background-color:greenyellow; font-size:40px; padding:0px 12px; color:blue; line-height:40px;',
+      color: 'Blue',  // Must be synced with firstPlayer.checkerCss's color value
+      checkerCss: 'background-color:greenyellow; font-size:40px; padding:0px 12px; color:blue; line-height:40px;',
       direction: 1
     },
     secondPlayer: {
-      color: 'Red',  // Must be synced with secondPlayer.checkerCSS's color value
-      checkerCSS: 'background-color:greenyellow; font-size:40px; padding:0px 12px; color:red; line-height:40px;',
+      color: 'Red',  // Must be synced with secondPlayer.checkerCss's color value
+      checkerCss: 'background-color:greenyellow; font-size:40px; padding:0px 12px; color:red; line-height:40px;',
       direction: -1
     },
-    openSquare: 'background-color:greenyellow; font-size:40px; padding:0px 12px; color:greenyellow; line-height:40px;',
-    blackSquare: 'background-color:black; font-size:40px; padding:0px 12px; color:black; line-height:40px;',
-    labelSquare: 'background-color:white; font-size:40px; padding:0px 12px; color:greenyellow; line-height:40px;',
-    cornerSquare: 'background-color:white; font-size:40px; padding:0px 12px; color:white; line-height:40px;'
+    squareCss: {
+      open: 'background-color:greenyellow; font-size:40px; padding:0px 12px; color:greenyellow; line-height:40px;',
+      black: 'background-color:black; font-size:40px; padding:0px 12px; color:black; line-height:40px;',
+      label: 'background-color:white; font-size:40px; padding:0px 12px; color:greenyellow; line-height:40px;',
+      corner: 'background-color:white; font-size:40px; padding:0px 12px; color:white; line-height:40px;'
+    }
   };
   this.grid = [];
   this.checkers = [];
@@ -102,18 +96,18 @@ function Board () {
       this.grid[row] = [];
       for (let column = 0; column < 8; column++) {
         if (flag) {
-          this.grid[row].push(this.constructor.blackSquare);
+          this.grid[row].push(this.constructor.squareCss.black);
           flag = !flag;
         } else {
           switch (true) {
             case row <= 2:
-              this.grid[row].push(this.constructor.firstPlayer.checkerCSS);
+              this.grid[row].push(this.constructor.firstPlayer.checkerCss);
               break;
             case row <= 4:
-              this.grid[row].push(this.constructor.openSquare);
+              this.grid[row].push(this.constructor.squareCss.open);
               break;
             default:
-              this.grid[row].push(this.constructor.secondPlayer.checkerCSS);
+              this.grid[row].push(this.constructor.secondPlayer.checkerCss);
           }
           flag = !flag;
         }
@@ -125,9 +119,9 @@ function Board () {
   // prints out the board
   this.viewGrid = function () {
     console.log('\n');
-    console.log('%cC' + '%c0' + '%c1' + '%c2' + '%c3' + '%c4' + '%c5' + '%c6' + '%c7', this.constructor.cornerSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare);
+    console.log('%cC' + '%c0' + '%c1' + '%c2' + '%c3' + '%c4' + '%c5' + '%c6' + '%c7', this.constructor.squareCss.corner, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label);
     for (let i = 0; i < 8; i++) {
-      console.log(`%c${i}` + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[i][0], this.grid[i][1], this.grid[i][2], this.grid[i][3], this.grid[i][4], this.grid[i][5], this.grid[i][6], this.grid[i][7]);
+      console.log(`%c${i}` + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[i][0], this.grid[i][1], this.grid[i][2], this.grid[i][3], this.grid[i][4], this.grid[i][5], this.grid[i][6], this.grid[i][7]);
     }
     console.log('\n');
   };
@@ -141,15 +135,15 @@ function move (whichPiece, toWhere) {
 }
 
 /*
-console.log('%cC' + '%c0' + '%c1' + '%c2' + '%c3' + '%c4' + '%c5' + '%c6' + '%c7', this.constructor.cornerSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare, this.constructor.labelSquare);
-console.log('%c0' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[0][0], this.grid[0][1], this.grid[0][2], this.grid[0][3], this.grid[0][4], this.grid[0][5], this.grid[0][6], this.grid[0][7]);
-console.log('%c1' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[1][0], this.grid[1][1], this.grid[1][2], this.grid[1][3], this.grid[1][4], this.grid[1][5], this.grid[1][6], this.grid[1][7]);
-console.log('%c2' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[2][0], this.grid[2][1], this.grid[2][2], this.grid[2][3], this.grid[2][4], this.grid[2][5], this.grid[2][6], this.grid[2][7]);
-console.log('%c3' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[3][0], this.grid[3][1], this.grid[3][2], this.grid[3][3], this.grid[3][4], this.grid[3][5], this.grid[3][6], this.grid[3][7]);
-console.log('%c4' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[4][0], this.grid[4][1], this.grid[4][2], this.grid[4][3], this.grid[4][4], this.grid[4][5], this.grid[4][6], this.grid[4][7]);
-console.log('%c5' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[5][0], this.grid[5][1], this.grid[5][2], this.grid[5][3], this.grid[5][4], this.grid[5][5], this.grid[5][6], this.grid[5][7]);
-console.log('%c6' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[6][0], this.grid[6][1], this.grid[6][2], this.grid[6][3], this.grid[6][4], this.grid[6][5], this.grid[6][6], this.grid[6][7]);
-console.log('%c7' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.labelSquare, this.grid[7][0], this.grid[7][1], this.grid[7][2], this.grid[7][3], this.grid[7][4], this.grid[7][5], this.grid[7][6], this.grid[7][7]);
+console.log('%cC' + '%c0' + '%c1' + '%c2' + '%c3' + '%c4' + '%c5' + '%c6' + '%c7', this.constructor.squareCss.corner, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label, this.constructor.squareCss.label);
+console.log('%c0' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[0][0], this.grid[0][1], this.grid[0][2], this.grid[0][3], this.grid[0][4], this.grid[0][5], this.grid[0][6], this.grid[0][7]);
+console.log('%c1' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[1][0], this.grid[1][1], this.grid[1][2], this.grid[1][3], this.grid[1][4], this.grid[1][5], this.grid[1][6], this.grid[1][7]);
+console.log('%c2' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[2][0], this.grid[2][1], this.grid[2][2], this.grid[2][3], this.grid[2][4], this.grid[2][5], this.grid[2][6], this.grid[2][7]);
+console.log('%c3' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[3][0], this.grid[3][1], this.grid[3][2], this.grid[3][3], this.grid[3][4], this.grid[3][5], this.grid[3][6], this.grid[3][7]);
+console.log('%c4' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[4][0], this.grid[4][1], this.grid[4][2], this.grid[4][3], this.grid[4][4], this.grid[4][5], this.grid[4][6], this.grid[4][7]);
+console.log('%c5' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[5][0], this.grid[5][1], this.grid[5][2], this.grid[5][3], this.grid[5][4], this.grid[5][5], this.grid[5][6], this.grid[5][7]);
+console.log('%c6' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[6][0], this.grid[6][1], this.grid[6][2], this.grid[6][3], this.grid[6][4], this.grid[6][5], this.grid[6][6], this.grid[6][7]);
+console.log('%c7' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf' + '%c\u25cf', this.constructor.squareCss.label, this.grid[7][0], this.grid[7][1], this.grid[7][2], this.grid[7][3], this.grid[7][4], this.grid[7][5], this.grid[7][6], this.grid[7][7]);
 console.log('\n');
 
   console.log('%cC' + '%c0' + '%c1' + '%c2' + '%c3' + '%c4' + '%c5' + '%c6' + '%c7', corner, label, label, label, label, label, label, label, label);
